ee703c8e4c1cd0fc10cf49ffdf60936f7e150ddf
MaiXuanCuong <maixuancuong2906@gmail.com>
2022-06-21 20:15:43 +0700

    M1

--- 003/snake.js
+++ 003/snake.js
@@ -1,54 +1,34 @@
 const cvs = document.getElementById("snake");
 const ctx = cvs.getContext("2d");
-
-// create the unit
 const box = 32;
-
-// load images
-
 const ground = new Image();
 ground.src = "./img/ground1.png"
 const foodImg = new Image();
 foodImg.src = "./img/food1.png";
-
-// load audio files
-
 let dead = new Audio();
 let eat = new Audio();
 let up = new Audio();
 let right = new Audio();
 let left = new Audio();
 let down = new Audio();
-
 dead.src = "audio/dead.mp3";
 eat.src = "audio/eat.mp3";
 up.src = "audio/up.mp3";
 right.src = "audio/right.mp3";
 left.src = "audio/left.mp3";
 down.src = "audio/down.mp3";
-
-// create the snake
-
 let snake = [];
-
 snake[0] = {
     x : 9 * box,
     y : 10 * box
 };
-
-// create the food
-
 let food = {
     x : Math.floor(Math.random()*17+1) * box,
     y : Math.floor(Math.random()*15+3) * box
 }
-
-// create the score var
 let score = 0;
-//control the snake
 let d;
 document.addEventListener("keydown",direction);
-
 function direction(event){
     let key = event.keyCode;
     if( key == 37 && d != "RIGHT"){
@@ -66,8 +46,8 @@ function direction(event){
     }
 }
 function click_play(){
-    return location.reload();}
-// cheack collision function
+    return location.reload();
+}
 function collision(head,array){
     for(let i = 0; i < array.length; i++){
         if(head.x == array[i].x && head.y == array[i].y){
@@ -76,35 +56,21 @@ function collision(head,array){
     }
     return false;
 }
-// draw everything to the canvas
 function draw(){
-    
     ctx.drawImage(ground,0,0);
-    
     for( let i = 0; i < snake.length ; i++){
         ctx.fillStyle = ( i == 0 )? "#00FF00" : "#FF99FF";
-        // "green" "white" "red"
         ctx.fillRect(snake[i].x,snake[i].y,box,box);
-        
         ctx.strokeStyle = "red";
         ctx.strokeRect(snake[i].x,snake[i].y,box,box);
-        // requestAnimationFrame(draw);
-
     }
-    
     ctx.drawImage(foodImg, food.x, food.y);
-    
-    // old head position
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
-    
-    // which direction
     if( d == "LEFT") snakeX -= box;
     if( d == "UP") snakeY -= box;
     if( d == "RIGHT") snakeX += box;
     if( d == "DOWN") snakeY += box;
-    
-    // if the snake eats the foodS
     if(snakeX == food.x && snakeY == food.y){
         score++;
         eat.play();
@@ -112,53 +78,27 @@ function draw(){
             x : Math.floor(Math.random()*17+1) * box,
             y : Math.floor(Math.random()*15+3) * box
         }
-        // we don't remove the tail
     }else{
-        // remove the tail
         snake.pop();
     }
-    
-    // add new Head
-    
     let newHead = {
         x : snakeX,
         y : snakeY
     }
-    
-    // game over
-    
     if(snakeX < box || snakeX > 17 * box || snakeY < 3*box || snakeY > 17*box || collision(newHead,snake)){
         clearInterval(game);
         dead.play();   
-      
         setTimeout(function(){  var xacnhan  =  confirm("GAME OVER: Điểm của bạn là " + score + " Bạn muốn chơi lại không?") 
         if (xacnhan == true) {
             click_play();
-        } },1000);
-        // alert("GAME OVER: Điểm của bạn là " + score) 
-      
-        
-            
-    
-        
+        } },1000)  
     }
-    
     snake.unshift(newHead);
-    
     ctx.fillStyle = "black";
     ctx.font = "50px Changa one";
     ctx.fillText("Điểm: "+score,2*box,1.6*box);
 } 
-var td = prompt("Nhập tốc độ trò chơi >200:chậm 100<:nhanh 50<:siêu nhanh")
-var game = setInterval(draw,td)
-
-  
-
-// call draw function every 100 ms
- 
-// = prompt("Nhập tốc độ trò chơi >200:chậm 100<:nhanh 50<:siêu nhanh")
-
-// draw();
+var game = setInterval(draw,90)
 
 
 
